a3979d1f0434626cdaac0db5f76e81574c15fc32
Miguel Acero <juanm.acero2020@gmail.com>
2026-08-20 08:17:51 -0500

    boton

--- Proyecto Estudiantes/Form1.Designer.cs
+++ Proyecto Estudiantes/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Proyecto_Estudiantes
 {
-    partial class Form1
+    public partial class Form1 : Form
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Pink;
+            button1.Location = new Point(150, 37);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "hola";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- Proyecto Estudiantes/Form1.cs
+++ Proyecto Estudiantes/Form1.cs
@@ -6,5 +6,10 @@ namespace Proyecto_Estudiantes
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
