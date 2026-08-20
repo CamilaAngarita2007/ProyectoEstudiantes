e51dac26655a4d0c930d62e4f453056315cbaac9
Miguel Acero <juanm.acero2020@gmail.com>
2026-08-20 08:56:34 -0500

    cambios

--- Proyecto Estudiantes/Form1.Designer.cs
+++ Proyecto Estudiantes/Form1.Designer.cs
@@ -28,33 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(227, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(189, 20);
+            label1.TabIndex = 1;
+            label1.Text = "SISTEMA DE ESTUDIANTES";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(68, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Edad:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(68, 89);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Nombre:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(196, 89);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(271, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(196, 141);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 5;
             // 
             // button1
             // 
-            button1.BackColor = Color.Pink;
-            button1.Location = new Point(150, 37);
+            button1.Location = new Point(78, 223);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "hola";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            button1.TabIndex = 6;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(227, 223);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 7;
+            button2.Text = "Limpiar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(386, 223);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 8;
+            button3.Text = "Eliminar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
         private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
