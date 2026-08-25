255d955ab1089a7d848eb6d3f636fc03cfdbfe5e
ADSO <brayancamilo7233@gmil.com>
2026-08-25 10:53:34 -0500

    Agregar label,

--- Proyecto Estudiantes/Form4.Designer.cs
+++ Proyecto Estudiantes/Form4.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -41,11 +46,63 @@
             label1.TabIndex = 0;
             label1.Text = "Formulario de Brayan";
             // 
+            // button1
+            // 
+            button1.Font = new Font("Vladimir Script", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.Location = new Point(85, 257);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(136, 117);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(238, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(136, 90);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(238, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Vladimir Script", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(60, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 19);
+            label2.TabIndex = 7;
+            label2.Text = "Nombre";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Vladimir Script", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(60, 117);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 19);
+            label3.TabIndex = 8;
+            label3.Text = "Edad";
+            // 
             // Form4
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
             Controls.Add(label1);
             Name = "Form4";
             Text = "Form4";
@@ -56,5 +113,10 @@
         #endregion
 
         private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
     }
 }
